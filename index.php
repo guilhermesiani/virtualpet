f8c31c249a85c99147a6f21389bf7c9aadcb5585
Guilherme Siani <guilherme@siani.com.br>
2015-08-02 15:40:18 -0300

    Autoload

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
 
 include 'configs/config.php';
-include 'libs/Bootstrap.php';
-include 'libs/Controller.php';
-include 'libs/Model.php';
-include 'libs/View.php';
-include 'libs/Filter.php';
+
+function autoload($className)
+{
+	$className = ltrim($className, '\\');
+	$fileName = '';
+	$namespace = '';
+
+	if ($lastNsPos = strpos($className, '\\')) {
+		$namespace = substr($className, 0, $lastNsPos);
+		$className = substr($className, $lastNsPos + 1);
+		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	}
+
+	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+	require $fileName;
+}
+
+spl_autoload_register('autoload');
+
 
 // Inserir namespaces e autoload
 $app = new Libs\Bootstrap();
