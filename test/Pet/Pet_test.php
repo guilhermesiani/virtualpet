f3921ea8e6135a81a89579c3c87541548b749639
Guilherme Siani <guilherme@siani.com.br>
2015-10-01 00:06:26 -0300

    Incluído novos testes e para as funções Feed e Play dos Pets e redefinido as já existentes.

--- test/Pet/Pet_test.php
+++ test/Pet/Pet_test.php
@@ -11,77 +11,125 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/libs/Pet/MartianPet/MartianFlyingPet.ph
 use Libs\Pet\MartianPet\MartianPet as MartianPet;
 use Libs\Pet\MartianPet\MartianFlyingPet as MartianFlyingPet;
 
+Mock::generate('Strawberry');
+Mock::generate('Ball');
+
 /**
 * 
 */
 class TestOfPet extends UnitTestCase
 {
-	private $pet;
-
-	function __construct() 
+	public function testInstanceWithValidDependencyShouldWork()
 	{
-		$this->pet = new MartianPet(new MartianFlyingPet);
+		$pet = new MartianPet(new MartianFlyingPet);
+		$this->assertIsA($pet, 'Libs\Pet\MartianPet\MartianPet');
+		$this->assertIsA($pet, 'Libs\Pet\Pet');
 	}
 
-	public function testConstruct()
+	public function testInstanceWithInvalidDependencyShouldThrownAnException()
 	{
-		$this->assertIsA($this->pet, 'Libs\Pet\MartianPet\MartianPet');
-		$this->assertIsA($this->pet, 'Libs\Pet\Pet');
-	}
+		$this->expectException();
 
-	public function testSetId()
-	{
-		$this->pet->setId(2);
-		$this->assertIdentical($this->pet->getId(), 2);
-	}
-
-	public function testSetName()
-	{
-		$this->pet->setName('Other name');
-		$this->assertIdentical($this->pet->getName(), 'Other name');
-	}
-
-	public function testSetAge()
-	{
-		$this->pet->setAge(3.56);
-		$this->assertIdentical($this->pet->getAge(), 3.56);
-	}
-
-	public function testSetHunger() 
-	{
-		$this->pet->setHunger(3);
-		$this->assertIdentical($this->pet->getHunger(), 3);
-	}
-
-	public function testSetStress() 
-	{
-		$this->pet->setStress(3);
-		$this->assertIdentical($this->pet->getStress(), 3);
-	}
-
-	public function testFeed()
-	{
-
-	}
-
-	public function testPlay()
-	{
-
+		$pet = new MartianPet('expect an exception');
 	}	
 
-	public function testDie() 
+	public function testSetIdShouldRetrieveAnEqualIntValueOnGet()
 	{
-		$this->assertIsA($this->pet->getAlive(), 'bool');
-		$this->pet->die();
-		$this->assertIsA($this->pet->getAlive(), 'bool');
-		$this->assertIdentical($this->pet->getAlive(), false);
+		$pet = new MartianPet(new MartianFlyingPet);		
+		$pet->setId(2);
+		$this->assertIdentical($pet->getId(), 2);
+	}
+
+	public function testSetNameShouldRetrieveAnEqualStringValueOnGet()
+	{
+		$pet = new MartianPet(new MartianFlyingPet);
+		$pet->setName('Other name');
+		$this->assertIdentical($pet->getName(), 'Other name');
+	}
+
+	public function testSetAgeShouldRetrieveAnEqualFloatValueOnGet()
+	{
+		$pet = new MartianPet(new MartianFlyingPet);
+		$pet->setAge(3.56);
+		$this->assertIdentical($pet->getAge(), 3.56);
+	}
+
+	public function testSetHungerWithIntValueShouldWork()
+	{
+		$pet = new MartianPet(new MartianFlyingPet);		
+		$pet->setHunger(3);
+		$this->assertIdentical($pet->getHunger(), 3);
+	}
+
+	public function testSetHungerWithFloatValueShouldWorkAndRoundUpConvertedInt()
+	{
+		$pet = new MartianPet(new MartianFlyingPet);
+		$pet->setHunger(3.7);
+		$this->assertIdentical($pet->getHunger(), 4);
 	}	
 
-	public function testGetOld()
+	public function testSetStressShouldRetrieveAnEqualIntValueOnGet() 
 	{
-		$this->assertIsA($this->pet->getAge(), 'float');
-		$this->pet->getOld(1.0);
-		$this->assertIsA($this->pet->getAge(), 'float');
-		$this->assertIdentical($this->pet->getAge(), 1.2);
+		$pet = new MartianPet(new MartianFlyingPet);		
+		$pet->setStress(3);
+		$this->assertIdentical($pet->getStress(), 3);
+	}
+
+	public function testFeedPetWithFoodInstanceShouldWork()
+	{
+		$strawberry = new MockStrawberry();
+		$strawberry->returns('getSatisfied', 3);
+
+		$pet = new MartianPet(new MartianFlyingPet);
+		$pet->setHunger(100);
+		$pet->feed($strawberry);
+		$this->assertEqual($pet->getHunger(), 97);
+	}
+
+	public function testFeedPetWithInvalidValueShouldThrowAnException()
+	{
+		$this->expectException();	
+
+		$ball = new MockBall();
+		$pet = new MartianPet(new MartianFlyingPet);
+		$pet->feed($ball);
+	}
+
+	public function testPlayWithPetWithToyInstanceShouldWork()
+	{
+		$ball = new MockBall();
+		$ball->returns('getHappy', 20);
+
+		$pet = new MartianPet(new MartianFlyingPet);
+		$pet->setStress(100);
+		$pet->play($ball);
+		$this->assertEqual($pet->getStress(), 80);
+	}
+
+	public function testPlayWithPetWithAnInvalidValueShouldThrowAnException()
+	{
+		$this->expectedException();	
+
+		$strawberry = new MockStrawberry();
+		$pet = new MartianPet(new MartianFlyingPet);
+		$pet->feed($strawberry);
+	}	
+
+	public function testPetDieShouldReturnFalseOnGetAlive() 
+	{
+		$pet = new MartianPet(new MartianFlyingPet);		
+		$this->assertIsA($pet->getAlive(), 'bool');
+		$pet->die();
+		$this->assertIsA($pet->getAlive(), 'bool');
+		$this->assertIdentical($pet->getAlive(), false);
+	}	
+
+	public function testPetShouldGetOldWithFloatValueWithDefinedFactor()
+	{
+		$pet = new MartianPet(new MartianFlyingPet);		
+		$this->assertIsA($pet->getAge(), 'float');
+		$pet->getOld(1.0);
+		$this->assertIsA($pet->getAge(), 'float');
+		$this->assertIdentical($pet->getAge(), 1.2);
 	}
 }
