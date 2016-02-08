e62d700f1ca4b35f70514b6c6db4856ffd8a4426
William Bianchini <wbianchini@outlook.com>
2016-02-08 21:28:20 -0200

    database constants

--- config.php
+++ config.php
@@ -5,3 +5,24 @@ require 'vendor/autoload.php';
 
 /* Some conf here */
 define('URL', 'http://localhost:9090/');
+
+
+/*
+ * Database Server
+ */
+define('DB_NAME', 'virtualpet');
+
+/*
+ * Database Server
+ */
+define('DB_HOST', 'localhost');
+
+/*
+ * Database User
+ */
+define('DB_USER', 'william');
+
+/*
+ * Database Password
+ */
+define('DB_PASSWORD', '');
